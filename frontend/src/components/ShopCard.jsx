6dadc11054251a68984469e2f0f2b21849b4291d
KeriaKH <151910742+KeriaKH@users.noreply.github.com>
2025-06-07 14:08:51 +0700

    feat: enhance pagination and shop/product card components; integrate data fetching for shops and products

--- frontend/src/components/ShopCard.jsx
+++ frontend/src/components/ShopCard.jsx
@@ -3,37 +3,51 @@ import food1 from "../assets/food1.jpg";
 import { faClock, faMoneyBill } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
-export default function ShopCard() {
-  const navigate=useNavigate()
+export default function ShopCard({ shopDetail }) {
+  const navigate = useNavigate();
 
-  function handleMove(){
-    navigate('shop')
+  function handleMove() {
+    navigate("shop");
   }
   return (
     <div className="flex flex-col rounded-2xl shadow-xl bg-white">
       <img src={food1} alt="" className="rounded-t-2xl" />
       <div className="p-2 space-y-1 flex flex-col justify-center">
-        <h3 className="font-bold text-lg">Cafe Amazone - Hai Bà Trưng</h3>
-        <p className="text-sm">95 Hai Bà Trưng, P.Bến Nghé, Quận 1, TP. HCM</p>
+        <h3 className="font-bold text-xl py-2 truncate">{shopDetail.name}</h3>
+        <p className="text-sm">{shopDetail.address}</p>
         <hr />
         <div className="flex items-center justify-between text-yellow-500">
           <div className="flex space-x-2">
-            <p>876+ </p>
-            <p> 5★ </p>
+            <p>{shopDetail.review}</p>
+            <p> {shopDetail.star} ★ </p>
           </div>
-          <button className="bg-[rgba(60,152,80,1)] border text-white p-1 px-2 rounded-2xl hover:opacity-80" onClick={handleMove}>
+          <button
+            className="bg-[rgba(60,152,80,1)] border text-white p-1 px-2 rounded-2xl hover:opacity-80"
+            onClick={handleMove}
+          >
             Đặt món
           </button>
         </div>
         <div className="flex justify-between items-center ">
-          <p className="text-[rgba(109,213,29,1)]">Mở cửa</p>
+          <p
+            className={`${
+              shopDetail.isActive ? "text-[rgba(109,213,29,1)]" : "text-red-600"
+            } flex items-center`}
+          >
+            <div
+              className={`size-3 rounded-full mr-2 ${
+                shopDetail.isActive ? "bg-[rgba(109,213,29,1)]" : "bg-red-600"
+              }`}
+            ></div>
+            {shopDetail.isActive ? "Mở cửa" : "Đóng cửa"}
+          </p>
           <div className="space-x-2 flex items-center">
             <FontAwesomeIcon icon={faClock} />
-            <p>7:00 - 22:00</p>
+            <p>{shopDetail.timeRange}</p>
           </div>
         </div>
         <p className="text-xl py-2">
-          <FontAwesomeIcon icon={faMoneyBill} /> 20.000 - 100.000
+          <FontAwesomeIcon icon={faMoneyBill} /> {shopDetail.priceRange}
         </p>
       </div>
     </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,33 +1,59 @@
+import { useEffect, useState } from "react";
 import Category from "../components/Category";
 import Pagination from "../components/Pagination";
 import ShopCard from "../components/ShopCard";
 
-
 export default function HomePage() {
+  const SHOP_LIMIT = 12;
+  const [page, setPage] = useState(1);
+  const [count, setCount] = useState(0);
+
   const categories = [
-    { name: 'Tất cả', color: 'bg-green-500' },
-    { name: 'Cơm', color: 'bg-orange-400' },
-    { name: 'Bún / Phở / Hủ tiếu', color: 'bg-red-400' },
-    { name: 'Cháo / Soup / Canh', color: 'bg-blue-300' },
-    { name: 'Bánh mì / Bánh cuốn', color: 'bg-pink-400' },
-    { name: 'Burger / Pizza', color: 'bg-blue-400' },
-    { name: 'Salad / Healthy', color: 'bg-green-400' },
+    { name: "Tất cả", color: "bg-green-500" },
+    { name: "Cơm", color: "bg-orange-400" },
+    { name: "Bún / Phở / Hủ tiếu", color: "bg-red-400" },
+    { name: "Cháo / Soup / Canh", color: "bg-blue-300" },
+    { name: "Bánh mì / Bánh cuốn", color: "bg-pink-400" },
+    { name: "Burger / Pizza", color: "bg-blue-400" },
+    { name: "Salad / Healthy", color: "bg-green-400" },
   ];
 
   const priceRanges = [
-  { name: 'Tất cả', color: 'bg-green-500' },
-  { name: 'Dưới 20 nghìn', color: 'bg-yellow-400' },
-  { name: '20 - 50 nghìn', color: 'bg-green-300' },
-  { name: '50 - 100 nghìn', color: 'bg-green-600' },
-  { name: 'Trên 100 nghìn', color: 'bg-green-700' },
-];
+    { name: "Tất cả", color: "bg-green-500" },
+    { name: "Dưới 20 nghìn", color: "bg-yellow-400" },
+    { name: "20 - 50 nghìn", color: "bg-green-300" },
+    { name: "50 - 100 nghìn", color: "bg-green-600" },
+    { name: "Trên 100 nghìn", color: "bg-green-700" },
+  ];
 
-const otherCriteria = [
-  { name: 'Tất cả', color: 'bg-green-500' },
-  { name: 'Được đánh giá cao',  color: 'bg-yellow-500' }, 
-  { name: 'Giao nhanh', color: 'bg-blue-500' },     
-  { name: 'Khuyến mãi', color: 'bg-pink-500' },       
-];
+  const otherCriteria = [
+    { name: "Tất cả", color: "bg-green-500" },
+    { name: "Được đánh giá cao", color: "bg-yellow-500" },
+    { name: "Giao nhanh", color: "bg-blue-500" },
+    { name: "Khuyến mãi", color: "bg-pink-500" },
+  ];
+  const [shops, setShops] = useState([]);
+
+  const fetchShops = async () => {
+    try {
+      const response = await fetch("/sampleData/shopDetail.json");
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        return {};
+      }
+    } catch (error) {
+      console.log("Error fetching shop data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchShops().then((res) => {
+      setShops(res.data);
+      setCount(res.count);
+    });
+  }, []);
   return (
     <div className="w-[80vw] mx-auto flex">
       <div className="w-[80%] space-y-3">
@@ -35,17 +61,23 @@ const otherCriteria = [
           Các nhà hàng phổ biến
         </h1>
         <hr className="w-[90%] mx-auto mb-3" />
-        <div className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-1 gap-7 mb-7">
-          {Array.from({ length: 20 }).map((_, index) => (
-            <ShopCard key={index} />
-          ))}
+        <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-10 mb-7">
+          {Array.isArray(shops) &&
+            shops.slice((page-1)*SHOP_LIMIT,page*SHOP_LIMIT).map((item, index) => (
+              <ShopCard key={index} shopDetail={item} />
+            ))}
         </div>
-        <Pagination />
+        <Pagination
+          limit={SHOP_LIMIT}
+          count={count}
+          current={page}
+          onPageChange={setPage}
+        />
       </div>
       <div className="space-y-3 mt-10 ml-4">
-        <Category header={'Danh mục món ăn'} items={categories}/>
-        <Category header={'Mức giá'} items={priceRanges}/>
-        <Category header={'Tiêu chí khác'} items={otherCriteria}/>
+        <Category header={"Danh mục món ăn"} items={categories} />
+        <Category header={"Mức giá"} items={priceRanges} />
+        <Category header={"Tiêu chí khác"} items={otherCriteria} />
       </div>
     </div>
   );
