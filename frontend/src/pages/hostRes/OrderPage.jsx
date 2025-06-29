608fa8f6aab150ff2978f65995bd1f4b5578e1b5
KeriaKH <151910742+KeriaKH@users.noreply.github.com>
2025-06-30 00:46:13 +0700

    fix: handle potential undefined totalItems in OrderPage and HistoryPage

--- frontend/src/pages/hostRes/OrderPage.jsx
+++ frontend/src/pages/hostRes/OrderPage.jsx
@@ -34,7 +34,7 @@ export default function OrderPage() {
       getOrderData(resId, user.token).then((res) => {
         console.log(res);
         setOrders(res.orders);
-        setCount(res.totalItems);
+        setCount(res.totalItems||0);
       });
       getDashboardData(resId, user.token).then((res) => {
         console.log(res);
@@ -91,7 +91,7 @@ export default function OrderPage() {
         </div>
       </div>
       <div className="bg-gray-200 p-3 px-5 rounded-xl flex items-center text-sm">
-        <p className="text-xl font-semibold">{orders.length} Đơn hàng</p>
+        <p className="text-xl font-semibold">{orders?.length} Đơn hàng</p>
         <div className="flex items-center ml-auto space-x-4">
           <div className="bg-white flex items-center p-1 space-x-2 rounded">
             <p>Trạng thái:</p>
